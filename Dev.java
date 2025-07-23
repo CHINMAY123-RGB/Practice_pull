25c60cab0f419d8bb64c73f874d78012ae4f9702
Chinmay <kchinmay294@gmail.com>
2025-07-23 16:10:16 +0530

    Adding sub logic.

--- Dev.java
+++ Dev.java
@@ -1,5 +1,8 @@
 calss Dev{
    public static void main(String [] args){
     System.out.println("Create brache for practice");
+    int a = 10;
+    int b = 20;
+    System.out.println("Sub b and a : " + (b-a));
    }
 }
