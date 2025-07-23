3262e45b9801ec754c1bbd56fae8db0ea33a59a4
Chinmay <kchinmay294@gmail.com>
2025-07-23 16:16:51 +0530

    I don't want to add main brach this changes.Revert "Adding sub logic."
    This reverts commit 25c60cab0f419d8bb64c73f874d78012ae4f9702.

--- Dev.java
+++ Dev.java
@@ -1,8 +1,5 @@
 calss Dev{
    public static void main(String [] args){
     System.out.println("Create brache for practice");
-    int a = 10;
-    int b = 20;
-    System.out.println("Sub b and a : " + (b-a));
    }
 }
